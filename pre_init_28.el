8514b1db66672f8b1ee896936c42f7ebc878b75f
Wan Ahmad Ardie Wan Razali <ahmadardie.r@redplanet.com.my>
2024-12-17 23:17:52 +0800

    first commit

---
+++ pre_init_28.el
@@ -0,0 +1,15 @@
+(progn
+  (custom-set-faces)
+  (setq user-emacs-directory "~/.config/emacs/")
+  ;;(add-to-list 'load-path (expand-file-name "elpa" user-emacs-directory))
+  (setq package-user-dir "~/.config/emacs/elpa")
+
+  (let ((default-directory "~/.config/emacs/elpa"))
+    (normal-top-level-add-subdirs-to-load-path))
+  (custom-set-variables
+   '(package-selected-packages
+     '(magik-mode doom-themes modus-themes auto-complete multiple-cursors powershell marginalia vertico use-package)))
+  
+
+  (load-file "~/my-own-lisp-files/init_28.el")
+  )
